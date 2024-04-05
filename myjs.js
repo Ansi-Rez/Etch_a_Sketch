daad6ef12436d9c122fbb37370417cb0ad8eed5a
Anson Reyes <ansonreyes12345@gmail.com>
2024-04-05 19:33:08 -0400

    Add user input functionality

--- myjs.js
+++ myjs.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
+//Initial Setting
 for(let i = 0; i < 256; i++)
 {
     const div = document.createElement("div");
@@ -9,3 +11,29 @@ for(let i = 0; i < 256; i++)
     });
     container.appendChild(div);
 }
+//Takes the users input and calculates the number of squares needed and the size of the squares
+button.addEventListener("click", ()=>
+{
+    let squares = parseInt(prompt("Please enter the number of squares per side between 2-100"));
+
+    //Removes previous divs
+    let previousSquares = container.querySelectorAll("div");
+
+    previousSquares.forEach((prevsquare)=>
+    {
+        container.removeChild(prevsquare);
+    });
+
+    //Adds new divs based on user input
+    for(let i = 0; i < (squares * squares); i++)
+    {
+        const div = document.createElement("div");
+        div.style.height = String(480/squares) + "px";
+        div.style.width = String(640/squares) + "px";
+        div.addEventListener("mouseover", function()
+        {
+            div.style.backgroundColor = "white";
+        });
+        container.appendChild(div);
+    }
+});
